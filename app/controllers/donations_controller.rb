56c13a6b1f9ce10d0d1149c54ad76ab4e7cf9f85
Taketai <83339671+Taketai@users.noreply.github.com>
2021-06-03 23:52:41 +0900

    新規登録実装完了

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,4 +1,27 @@
 class DonationsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @donation = Donation.order("created_at DESC")
   end
+
+
+
+  def new
+    @donation = Donation
+  end
+
+  def create
+
+    @donation = Donation.new(Donation_params)
+      if @donation.valid?
+        @donation.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+
+
 end
