2ae9225e2549a2ce3276f8bd437fddd112cab085
Taketai <83339671+Taketai@users.noreply.github.com>
2021-06-03 21:10:35 +0900

    ある程度のトップページ見た目実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'donations/index'
+  devise_for :users
+  root to: 'donations#index'
+  resources :donations
 end
