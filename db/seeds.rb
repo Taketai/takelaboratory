d010da3caf77d16fd7f757bf51a9218b470fb480
Taketai <83339671+Taketai@users.noreply.github.com>
2021-06-11 14:35:27 +0900

    実装中断m(_ _)m

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Livechat.create(text:'テストです〜〜')
+Livechat.create(text:'回線が不安定だ〜〜')
+Livechat.create(text:'始めて見に来ました〜〜')
+Livechat.create(text:'声が小さくて聞き取り辛いです・・・')
+Livechat.create(text:'もう少しゆっくり話して欲しいですm(_ _)m')
+Livechat.create(text:'プログラミングが楽しくなってきました♫')
