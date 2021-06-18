1abade6b6f31ceb5e935cbfdaed6ac22e4fc990c
Taketai <83339671+Taketai@users.noreply.github.com>
2021-06-18 16:46:53 +0900

    ランキング実装できたよ！

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,6 @@
 class QuizzesController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index
 
   def index
     @quiz = Quiz.order("created_at DESC")
@@ -11,7 +12,7 @@ class QuizzesController < ApplicationController
   def create
     @quiz = Quiz.new(quiz_params)
 
-    if @quiz.valid? && @quiz.answer == "いぬもあるけばぼうにあたる"
+    if @quiz.valid? && @quiz.answer == "ねこ"
       @quiz.save
       render :create
     else
@@ -20,10 +21,26 @@ class QuizzesController < ApplicationController
     end
   end
 
+  def search
+    @quizzes = Quiz.search(params[:keyword])
+    @users = User.all
+    # @quizzes = Quiz.search(ranking_params)
+  end
 
   private
   def quiz_params
     params.require(:quiz).permit(:answer).merge(user_id: current_user.id)
+  end
+
+
+  # def ranking_params
+  #   params.require(:quiz).permit(:answer)
+  # end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
