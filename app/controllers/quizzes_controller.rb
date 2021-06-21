c4b88e571ee13efbb96c8b7377ac626dffca9af4
Taketai <83339671+Taketai@users.noreply.github.com>
2021-06-21 10:12:04 +0900

    クイズランキング実装完了

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,7 @@
 class QuizzesController < ApplicationController
   before_action :authenticate_user!
   before_action :move_to_index
+  # before_action :move_to_ranking, only: [:index]
 
   def index
     @quiz = Quiz.order("created_at DESC")
@@ -24,23 +25,26 @@ class QuizzesController < ApplicationController
   def search
     @quizzes = Quiz.search(params[:keyword])
     @users = User.all
-    # @quizzes = Quiz.search(ranking_params)
   end
 
   private
+  
   def quiz_params
     params.require(:quiz).permit(:answer).merge(user_id: current_user.id)
   end
-
-
-  # def ranking_params
-  #   params.require(:quiz).permit(:answer)
-  # end
 
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
   end
+
+  # def move_to_ranking
+  #   @quizzes = Quiz.search(params[:user_id])
+  #   if @quizzes.present?
+  #     redirect_to action: :index
+  #   end
+  #     redirect_to action: :search
+  # end
 
 end
