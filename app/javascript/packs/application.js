618f216bed399f1636ad4339a914bd1b59225d0f
Taketai <83339671+Taketai@users.noreply.github.com>
2021-06-23 21:48:47 +0900

    一旦終了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,8 @@ require("../donation_price")
 require("../card")
 require("../oekaki")
 require("../registration_botton")
-require("../test")
 require('jquery')
-require('../chat')
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
